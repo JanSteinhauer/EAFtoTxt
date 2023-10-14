0f1c36fab186766a249f6c6ff7bb7d89dd4b5110
JanSteinhauer <insta.steini14@gmail.com>
2023-10-14 17:10:55 -0500

    adjust sorting out list

--- eafConverter.py
+++ eafConverter.py
@@ -3,11 +3,30 @@ import glob
 import re
 
 def extract_and_print_annotation_values(directory_path: str):
-    delimiter = 'TIER_ID="Translation Speaker'
+    delimiter = 'TIER_ID'
     annotation_pattern = re.compile(r'<ANNOTATION_VALUE>(.*?)</ANNOTATION_VALUE>', re.DOTALL)
     brackets_pattern = re.compile(r'\[.*?\]')
     question_pattern = re.compile(r'\(\?\?\?\)')
     laughs_pattern = re.compile(r'\(laughs\)')
+    caps_pattern = re.compile(r'\b[A-Z]+\b')  # To match words in all caps.
+
+    def clean_values(annotation_values):
+        unwanted_strings = ['eng', 'deu', 'amb', 'xxx', '#', '&amp;']
+
+        cleaned = [brackets_pattern.sub('', value) for value in annotation_values]
+        cleaned = [question_pattern.sub('', value) for value in cleaned]
+        cleaned = [laughs_pattern.sub('', value) for value in cleaned]
+        cleaned = [caps_pattern.sub('', value) for value in cleaned]
+        cleaned = [value.replace('%', '') for value in cleaned]  # Remove % characters
+        cleaned = [value.replace('\n', ' ') for value in cleaned]
+
+        # Remove unwanted strings
+        for unwanted in unwanted_strings:
+            cleaned = [value.replace(unwanted, '') for value in cleaned]
+        
+        cleaned = [value.strip() for value in cleaned]  # Removing leading and trailing spaces.
+        cleaned = [value for value in cleaned if value]  # filtering out empty strings
+        return cleaned
 
     for foldername, subfolders, filenames in os.walk(directory_path):
         for filename in glob.glob(os.path.join(foldername, '*.txt')):
@@ -24,11 +43,7 @@ def extract_and_print_annotation_values(directory_path: str):
 
                 for i, segment in enumerate(segments, start=1):
                     annotation_values = annotation_pattern.findall(segment)
-                    cleaned_annotation_values = [brackets_pattern.sub('', value) for value in annotation_values]
-                    cleaned_annotation_values = [question_pattern.sub('', value) for value in cleaned_annotation_values]
-                    cleaned_annotation_values = [laughs_pattern.sub('', value) for value in cleaned_annotation_values]
-                    cleaned_annotation_values = [value.replace('\n', ' ') for value in cleaned_annotation_values]
-                    cleaned_annotation_values = [value for value in cleaned_annotation_values if value]  # filtering out empty strings
+                    cleaned_annotation_values = clean_values(annotation_values)
 
                     if i % 2 != 0:  # Assuming odd segments are in German
                         german_segments.extend(cleaned_annotation_values)
