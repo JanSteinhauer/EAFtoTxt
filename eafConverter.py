98e55775fb263379f80b1353a83a4a1e03e2bf5e
JanSteinhauer <insta.steini14@gmail.com>
2023-10-15 11:47:36 -0500

    code refactored multiple files possible

--- eafConverter.py
+++ eafConverter.py
@@ -17,16 +17,18 @@ def extract_and_print_annotation_values(directory_path: str):
         cleaned = [question_pattern.sub('', value) for value in cleaned]
         cleaned = [laughs_pattern.sub('', value) for value in cleaned]
         cleaned = [caps_pattern.sub('', value) for value in cleaned]
-        cleaned = [value.replace('%', '') for value in cleaned]  # Remove % characters
+        cleaned = [value.replace('%', '') for value in cleaned]
         cleaned = [value.replace('\n', ' ') for value in cleaned]
 
-        # Remove unwanted strings
         for unwanted in unwanted_strings:
             cleaned = [value.replace(unwanted, '') for value in cleaned]
-        
-        cleaned = [value.strip() for value in cleaned]  # Removing leading and trailing spaces.
-        cleaned = [value for value in cleaned if value]  # filtering out empty strings
+
+        cleaned = [value.strip() for value in cleaned]
+        cleaned = [value for value in cleaned if value]
         return cleaned
+
+    german_segments = []
+    english_segments = []
 
     for foldername, subfolders, filenames in os.walk(directory_path):
         for filename in glob.glob(os.path.join(foldername, '*.eaf')):
@@ -36,17 +38,15 @@ def extract_and_print_annotation_values(directory_path: str):
 
                 if len(segments) < 2:
                     print(f"Skipping {filename} as it doesn't contain enough segments")
-                    continue  # skip files with less than 2 segments
+                    continue
 
-                german_segments = []
-                english_segments = []
+
                 trash = []
 
                 for i, segment in enumerate(segments, start=1):
                     annotation_values = annotation_pattern.findall(segment)
                     cleaned_annotation_values = clean_values(annotation_values)
 
-                    # Displaying the cleaned_annotation_values
                     print(f"Annotation values: {cleaned_annotation_values}")
 
                     user_input = input("Press e for English, g for German, or t for Trash: ").strip().lower()
@@ -59,10 +59,10 @@ def extract_and_print_annotation_values(directory_path: str):
                     elif user_input == 't':
                         trash.extend(cleaned_annotation_values)
 
-                with open('german_translation.txt', 'a', encoding='utf-8') as output_file:
-                    output_file.write(' '.join(german_segments))
-                with open('english_original.txt', 'a', encoding='utf-8') as output_file:
-                    output_file.write('\n'.join(english_segments))
+    with open('english_original.txt', 'a', encoding='utf-8') as output_file:
+        output_file.write('\n'.join(english_segments))
+    with open('german_translation.txt', 'a', encoding='utf-8') as output_file:
+        output_file.write(' '.join(german_segments))
 
 if __name__ == "__main__":
     current_directory = os.getcwd()
