1a478edd79555995ac2b23de0c94aeaa37fcb3c2
JanSteinhauer <insta.steini14@gmail.com>
2023-10-15 14:27:42 -0500

    now the converstion is totally automatic

--- README.md
+++ README.md
@@ -6,5 +6,4 @@ This is created for the Texas German Dialect Project
 2. add eaf files to the folder
 3. type into the console python3 eafConverter.py
 4. run python script
-5. if the text is German press g, if English e and trash t
 6. enjoy your new cleaned data :)
--- eafConverter.py
+++ eafConverter.py
@@ -2,8 +2,24 @@ import os
 import glob
 import re
 
+delimiters = [
+    'TIER_ID="TRANSCRIPTION_Speaker', 
+    'TIER_ID="Tokenization_Speaker', 
+    'TIER_ID="Language_Speaker', 
+    'TIER_ID="Normalization_Speaker', 
+    'TIER_ID="LEMMA_Speaker', 
+    'TIER_ID="POS_Speaker', 
+    'TIER_ID="TRANSCRIPTION_Interviewer', 
+    'TIER_ID="Tokenization_Interviewer', 
+    'TIER_ID="Language_Interviewer', 
+    'TIER_ID="Normalization_Interviewer', 
+    'TIER_ID="LEMMA_Interviewer', 
+    'TIER_ID="POS_Interviewer', 
+    'TIER_ID="TRANSLATION_Speaker', 
+    'TIER_ID="TRANSLATION_Interviewer'
+]
+
 def extract_and_print_annotation_values(directory_path: str):
-    delimiter = 'TIER_ID'
     annotation_pattern = re.compile(r'<ANNOTATION_VALUE>(.*?)</ANNOTATION_VALUE>', re.DOTALL)
     brackets_pattern = re.compile(r'\[.*?\]')
     question_pattern = re.compile(r'\(\?\?\?\)')
@@ -28,41 +44,52 @@ def extract_and_print_annotation_values(directory_path: str):
         return cleaned
 
     german_segments = []
+    german_segments_sentences = []
     english_segments = []
+
+    def split_by_delimiters(content, delimiters):
+        segments = [content]  # Start with the whole content as one segment
+        for delimiter in delimiters:
+            pieces = []
+            for segment in segments:
+                pieces.extend(segment.split(delimiter))
+            segments = pieces
+        return segments
 
     for foldername, subfolders, filenames in os.walk(directory_path):
         for filename in glob.glob(os.path.join(foldername, '*.eaf')):
             with open(filename, 'r', encoding='utf-8', errors='ignore') as file:
                 content = file.read()
-                segments = content.split(delimiter)
+                segments = split_by_delimiters(content, delimiters)
 
                 if len(segments) < 2:
                     print(f"Skipping {filename} as it doesn't contain enough segments")
                     continue
-
-
-                trash = []
+                print(len(segments))
+               
+ 
 
                 for i, segment in enumerate(segments, start=1):
                     annotation_values = annotation_pattern.findall(segment)
                     cleaned_annotation_values = clean_values(annotation_values)
 
-                    print(f"Annotation values: {cleaned_annotation_values}")
 
-                    user_input = input("Press e for English, g for German, or t for Trash: ").strip().lower()
-                    if user_input == 'e':
-                        english_segments.extend(cleaned_annotation_values)
-                        english_segments.append('\n')
-                    elif user_input == 'g':
+                    if (i == 2) ^ (i == 8):
+                        german_segments_sentences.extend(cleaned_annotation_values)
+                        german_segments_sentences.append('\n')
+                    if i in [2, 3, 5, 6, 8, 9, 11, 12]:
                         german_segments.extend(cleaned_annotation_values)
                         german_segments.append('\n')
-                    elif user_input == 't':
-                        trash.extend(cleaned_annotation_values)
+                    if i in [14,15]:
+                        english_segments.extend(cleaned_annotation_values)
+                        english_segments.append('\n')
 
-    with open('english_original.txt', 'a', encoding='utf-8') as output_file:
-        output_file.write('\n'.join(english_segments))
     with open('german_translation.txt', 'a', encoding='utf-8') as output_file:
         output_file.write(' '.join(german_segments))
+    with open('german_sentence_translation.txt', 'a', encoding='utf-8') as output_file:
+        output_file.write(' '.join(german_segments_sentences))
+    with open('english_translation.txt', 'a', encoding='utf-8') as output_file:
+        output_file.write(' '.join(english_segments))
 
 if __name__ == "__main__":
     current_directory = os.getcwd()
